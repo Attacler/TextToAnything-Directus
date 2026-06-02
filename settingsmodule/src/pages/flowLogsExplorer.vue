b0c4c8f0323f247303ff13c192728f0034f9ed3e
Attacler <bartdon1@outlook.com>
2026-06-02 19:14:28 +0200

    1.7.9:  - Add a loading indicator to the flow log explorer

--- settingsmodule/src/pages/flowLogsExplorer.vue
+++ settingsmodule/src/pages/flowLogsExplorer.vue
@@ -10,262 +10,288 @@ const { useFlowsStore } = useStores();
 const api = useApi();
 const flowStore = useFlowsStore();
 const currentFlow = ref(),
-  logs = ref<any[]>([]),
-  fromDate = ref(),
-  toDate = ref(),
-  inputFilter = ref(""),
-  statusFilter = ref("all"),
-  currentLog = ref(),
-  sortOrder = ref("Descending");
+    logs = ref<any[]>([]),
+    fromDate = ref(),
+    toDate = ref(),
+    inputFilter = ref(""),
+    statusFilter = ref("all"),
+    currentLog = ref(),
+    sortOrder = ref("Descending"),
+    loading = ref(false);
 
 onMounted(() => {
-  const aWeekAgo = new Date();
-  aWeekAgo.setDate(aWeekAgo.getDate() - 7);
-  fromDate.value = aWeekAgo.toISOString().split("T")[0];
-  toDate.value = new Date().toISOString().split("T")[0];
+    const aWeekAgo = new Date();
+    aWeekAgo.setDate(aWeekAgo.getDate() - 7);
+    fromDate.value = aWeekAgo.toISOString().split("T")[0];
+    toDate.value = new Date().toISOString().split("T")[0];
 });
 
 async function fetchLogs() {
-  const fetchLogs = await api.get("/revisions", {
-    params: {
-      "filter[_and][0][collection][_eq]": "directus_flows",
-      "filter[_and][1][item][_eq]": currentFlow.value || undefined,
-      "filter[_and][2][version][_null]": true,
-      "filter[_and][3][activity][action][_eq]": "run",
-      "filter[_and][4][activity][timestamp][_gte]": fromDate.value
-        ? fromDate.value
-        : undefined,
-      "filter[_and][4][activity][timestamp][_lte]": toDate.value
-        ? toDate.value + "T23:59:59"
-        : undefined,
-      "fields[0]": "*",
-      "fields[1]": "activity.*",
-      limit: 2000,
-      sort:
-        sortOrder.value == "Descending"
-          ? ["-activity.timestamp"]
-          : ["activity.timestamp"],
-    },
-  });
+    loading.value = true;
+    const fetchLogs = await api.get("/revisions", {
+        params: {
+            "filter[_and][0][collection][_eq]": "directus_flows",
+            "filter[_and][1][item][_eq]": currentFlow.value || undefined,
+            "filter[_and][2][version][_null]": true,
+            "filter[_and][3][activity][action][_eq]": "run",
+            "filter[_and][4][activity][timestamp][_gte]": fromDate.value
+                ? fromDate.value
+                : undefined,
+            "filter[_and][4][activity][timestamp][_lte]": toDate.value
+                ? toDate.value + "T23:59:59"
+                : undefined,
+            "fields[0]": "*",
+            "fields[1]": "activity.*",
+            limit: 2000,
+            sort:
+                sortOrder.value == "Descending"
+                    ? ["-activity.timestamp"]
+                    : ["activity.timestamp"],
+        },
+    });
 
-  logs.value = fetchLogs.data.data;
+    logs.value = fetchLogs.data.data;
+    loading.value = false;
 }
 
 const filteredLogs = computed(() => {
-  if (!inputFilter.value && statusFilter.value == "all") return logs.value;
+    if (!inputFilter.value && statusFilter.value == "all") return logs.value;
 
-  let logsToFilter = logs.value;
+    let logsToFilter = logs.value;
 
-  if (statusFilter.value != "all") {
-    logsToFilter = logsToFilter.filter((log) => {
-      const findLastStepKey = log.data.steps[log.data.steps.length - 1];
-      return (
-        findLastStepKey.status ==
-        (statusFilter.value == "error" ? "reject" : "resolve")
-      );
-    });
-  }
+    if (statusFilter.value != "all") {
+        logsToFilter = logsToFilter.filter((log) => {
+            const findLastStepKey = log.data.steps[log.data.steps.length - 1];
+            return (
+                findLastStepKey.status ==
+                (statusFilter.value == "error" ? "reject" : "resolve")
+            );
+        });
+    }
 
-  if (inputFilter.value) {
-    logsToFilter = logs.value.filter((log) => {
-      const input = log.data.data.$trigger;
-      return JSON.stringify(input)
-        .toLowerCase()
-        .includes(inputFilter.value.toLowerCase());
-    });
-  }
+    if (inputFilter.value) {
+        logsToFilter = logs.value.filter((log) => {
+            const input = log.data.data.$trigger;
+            return JSON.stringify(input)
+                .toLowerCase()
+                .includes(inputFilter.value.toLowerCase());
+        });
+    }
 
-  return logsToFilter;
+    return logsToFilter;
 });
 
 const flows = computed(() => {
-  return flowStore.flows.map((e: any) => ({ text: e.name, value: e.id }));
+    return flowStore.flows.map((e: any) => ({ text: e.name, value: e.id }));
 });
 
 function getOutput(log: any) {
-  if (log.data.steps.length == 0) return "";
+    if (log.data.steps.length == 0) return "";
 
-  const findLastStepKey = log.data.steps[log.data.steps.length - 1]?.key;
+    const findLastStepKey = log.data.steps[log.data.steps.length - 1]?.key;
 
-  if (!findLastStepKey) return "";
+    if (!findLastStepKey) return "";
 
-  return log.data.data[findLastStepKey];
+    return log.data.data[findLastStepKey];
 }
 
 function openLog(log: any) {
-  currentLog.value = log.item;
+    currentLog.value = log.item;
 }
 
 function isError(log: any) {
-  if (log.data.steps.length == 0) return false;
+    if (log.data.steps.length == 0) return false;
 
-  const findLastStepKey = log.data.steps[log.data.steps.length - 1];
+    const findLastStepKey = log.data.steps[log.data.steps.length - 1];
 
-  return findLastStepKey.status != "resolve";
+    return findLastStepKey.status != "resolve";
 }
 </script>
 
 <template>
-  <private-view title="Flow logs explorer">
-    <template #navigation>
-      <TTAnav />
-    </template>
-
-    <div class="px-5 w-full">
-      <v-table
-        :headers="[
-          {
-            text: 'Status',
-            value: 'status',
-            sortable: false,
-            width: 100,
-          },
-          {
-            text: 'Timestamp',
-            value: 'timestamp',
-            sortable: false,
-            width: 100,
-          },
-          {
-            text: 'Input',
-            value: 'input',
-            sortable: false,
-            width: 1000,
-          },
-          {
-            text: 'Output',
-            value: 'output',
-            sortable: false,
-            width: 1000,
-          },
-        ]"
-        :items="filteredLogs"
-        @click:row="openLog"
-      >
-        <template #item.status="{ item }">
-          <v-icon
-            :name="isError(item) ? 'error' : 'check_circle'"
-            class="flowLogStatus"
-            :class="{ error: isError(item), success: !isError(item) }"
-          />
+    <private-view title="Flow logs explorer">
+        <template #navigation>
+            <TTAnav />
         </template>
-        <template #item.timestamp="{ item }">
-          {{ new Date(item.activity.timestamp).toLocaleDateString() }}
+
+        <div class="px-5 w-full">
+            <v-table
+                :headers="[
+                    {
+                        text: 'Status',
+                        value: 'status',
+                        sortable: false,
+                        width: 100,
+                    },
+                    {
+                        text: 'Timestamp',
+                        value: 'timestamp',
+                        sortable: false,
+                        width: 100,
+                    },
+                    {
+                        text: 'Input',
+                        value: 'input',
+                        sortable: false,
+                        width: 1000,
+                    },
+                    {
+                        text: 'Output',
+                        value: 'output',
+                        sortable: false,
+                        width: 1000,
+                    },
+                ]"
+                :items="filteredLogs"
+                @click:row="openLog"
+            >
+                <template #item.status="{ item }">
+                    <v-icon
+                        :name="isError(item) ? 'error' : 'check_circle'"
+                        class="flowLogStatus"
+                        :class="{
+                            error: isError(item),
+                            success: !isError(item),
+                        }"
+                    />
+                </template>
+                <template #item.timestamp="{ item }">
+                    {{ new Date(item.activity.timestamp).toLocaleDateString() }}
+                </template>
+                <template #item.input="{ item }">
+                    <div
+                        :title="
+                            JSON.stringify(item.data.data.$trigger, null, 2)
+                        "
+                    >
+                        {{ item.data.data.$trigger }}
+                    </div>
+                </template>
+                <template #item.output="{ item }">
+                    <div :title="JSON.stringify(getOutput(item), null, 2)">
+                        {{ getOutput(item) }}
+                    </div>
+                </template>
+            </v-table>
+        </div>
+
+        <template #sidebar>
+            <div class="filters">
+                <h2>Filter</h2>
+
+                <div>
+                    <div class="field-name">Flow</div>
+                    <v-select
+                        :items="flows"
+                        showDeselect
+                        v-model="currentFlow"
+                    />
+                </div>
+
+                <div>
+                    <div class="field-name">From date</div>
+                    <dateTimePicker
+                        v-model="fromDate"
+                        :value="fromDate"
+                        v-if="fromDate"
+                    />
+                </div>
+                <div>
+                    <div class="field-name">To date</div>
+                    <dateTimePicker
+                        v-model="toDate"
+                        :value="toDate"
+                        v-if="toDate"
+                    />
+                </div>
+                <div>
+                    <div class="field-name">Sorting order</div>
+                    <v-select
+                        v-model="sortOrder"
+                        :items="['Descending', 'Ascending']"
+                    />
+                </div>
+                <v-button
+                    @click="fetchLogs"
+                    :fullWidth="true"
+                    :loading="loading"
+                    >Search</v-button
+                >
+
+                <div>
+                    Input filter
+                    <v-input
+                        v-model="inputFilter"
+                        placeholder="Input filter"
+                        class="w-full"
+                    />
+                </div>
+                <div>
+                    Status
+                    <VSelect
+                        v-model="statusFilter"
+                        :items="[
+                            {
+                                text: 'All',
+                                value: 'all',
+                            },
+                            {
+                                text: 'Error',
+                                value: 'error',
+                            },
+                            {
+                                text: 'Success',
+                                value: 'success',
+                            },
+                        ]"
+                    />
+                </div>
+            </div>
+
+            <displayFlowLogs
+                v-if="currentLog"
+                @close="currentLog = null"
+                :currentLog="currentLog"
+            />
         </template>
-        <template #item.input="{ item }">
-          <div :title="JSON.stringify(item.data.data.$trigger, null, 2)">
-            {{ item.data.data.$trigger }}
-          </div>
-        </template>
-        <template #item.output="{ item }">
-          <div :title="JSON.stringify(getOutput(item), null, 2)">
-            {{ getOutput(item) }}
-          </div>
-        </template>
-      </v-table>
-    </div>
-
-    <template #sidebar>
-      <div class="filters">
-        <h2>Filter</h2>
-
-        <div>
-          <div class="field-name">Flow</div>
-          <v-select :items="flows" showDeselect v-model="currentFlow" />
-        </div>
-
-        <div>
-          <div class="field-name">From date</div>
-          <dateTimePicker
-            v-model="fromDate"
-            :value="fromDate"
-            v-if="fromDate"
-          />
-        </div>
-        <div>
-          <div class="field-name">To date</div>
-          <dateTimePicker v-model="toDate" :value="toDate" v-if="toDate" />
-        </div>
-        <div>
-          <div class="field-name">Sorting order</div>
-          <v-select v-model="sortOrder" :items="['Descending', 'Ascending']" />
-        </div>
-        <v-button @click="fetchLogs" :fullWidth="true">Search</v-button>
-
-        <div>
-          Input filter
-          <v-input
-            v-model="inputFilter"
-            placeholder="Input filter"
-            class="w-full"
-          />
-        </div>
-        <div>
-          Status
-          <VSelect
-            v-model="statusFilter"
-            :items="[
-              {
-                text: 'All',
-                value: 'all',
-              },
-              {
-                text: 'Error',
-                value: 'error',
-              },
-              {
-                text: 'Success',
-                value: 'success',
-              },
-            ]"
-          />
-        </div>
-      </div>
-
-      <displayFlowLogs
-        v-if="currentLog"
-        @close="currentLog = null"
-        :currentLog="currentLog"
-      />
-    </template>
-  </private-view>
+    </private-view>
 </template>
 
 <style scoped>
 .w-full {
-  width: 100%;
+    width: 100%;
 }
 
 .px-5 {
-  padding: 0 2.5rem;
+    padding: 0 2.5rem;
 }
 
 .date-input {
-  min-width: 300px;
+    min-width: 300px;
 }
 
 .filters {
-  padding: 10px 5px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+    padding: 10px 5px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
 }
 
 .filters h2 {
-  font-size: 1.5rem;
-  font-weight: bold;
+    font-size: 1.5rem;
+    font-weight: bold;
 }
 
 .grow,
 .grow button {
-  flex-grow: 1;
+    flex-grow: 1;
 }
 
 .flowLogStatus.success {
-  color: var(--primary);
+    color: var(--primary);
 }
 
 .flowLogStatus.error {
-  color: var(--red);
+    color: var(--red);
 }
 </style>
